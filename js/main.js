09794cf5e2e3e9dd6c9b9fa75e1515b14ba4574c
GitGlor <karadzic.dusan@gmail.com>
2023-09-28 14:51:00 +0200

    mini prepravka pozicije elemanata

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Unix timestamp (in seconds) to count down to
-    var dayOfTheTrip = new Date('October 30, 2023 14:20:00').getTime() / 1000;
-  
-    // Set up FlipDown
-    var flipdown = new FlipDown(dayOfTheTrip)
-  
-      // Start the countdown
-      .start()
-  
-      // Do something when the countdown ends
-      .ifEnded(() => {
-        console.log('The countdown has ended!');
-      });
-  
-    // Toggle theme
-    var interval = setInterval(() => {
-      let body = document.body;
-      body.classList.toggle('light-theme');
-      body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
-      body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
-    }, 5000);
-  
-    // Show version number
-    var ver = document.getElementById('ver');
-    ver.innerHTML = flipdown.version;
+  // Unix timestamp (in seconds) to count down to
+  var dayOfTheTrip = new Date('October 30, 2023 14:20:00').getTime() / 1000;
+
+  // Set up FlipDown
+  var flipdown = new FlipDown(dayOfTheTrip)
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log('The countdown has ended!');
+    });
+
+  // Toggle theme
+  var interval = setInterval(() => {
+    let body = document.body;
+    body.classList.toggle('light-theme');
+    body.querySelector('#flipdown').classList.toggle('flipdown__theme-dark');
+    body.querySelector('#flipdown').classList.toggle('flipdown__theme-light');
+  }, 5000);
+
+  var accessButton = document.getElementById('accessButton');
+
+  accessButton.addEventListener('click', function () {
+    window.location.href = 'work-in-progress.html'
   });
+});
