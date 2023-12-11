45f9b8d2d569a534f9e743b68b7743a33b072242
GitGlor <karadzic.dusan@gmail.com>
2023-12-11 04:20:20 +0100

    uproscena verzija

--- js/map.js
+++ js/map.js
@@ -1,12 +1,21 @@
-// Customize map style here
-map.on('load', function () {
-    // Define the bounding box for South America
-    var southAmericaBounds = [
-        [-81, -57], // Southwest coordinates
-        [-52, 14]    // Northeast coordinates
-    ];
+// Coordinates for Novi Sad
+var noviSadCoordinates = [19.8416, 45.2671];
 
-    map.setMaxBounds(southAmericaBounds);
+// Create a marker
+var marker = new mapboxgl.Marker()
+    .setLngLat(noviSadCoordinates)
+    .addTo(map);
 
-    map.dragRotate.disable();
+// Add a click event listener to the marker
+marker.getElement().addEventListener('click', function () {
+    // Create a popup with an embedded YouTube video
+    var popup = new mapboxgl.Popup()
+        .setHTML(
+            '<iframe width="400" height="225" src="https://www.youtube.com/embed/ZaJulqeHKMg" frameborder="0" allowfullscreen></iframe>'
+        );
+
+    // Display the popup
+    marker.setPopup(popup);
+    popup.addTo(map);
+
 });
